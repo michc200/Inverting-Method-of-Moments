429f4d26a3b38a39397d700458e916d396f7503c
Michael Cohen <michc200@gmail.com>
2025-03-19 09:14:35 +0200

    slight changes to the moment functions

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import time
 
-def get_autocorrelation(signal: np.ndarray) -> np.ndarray:
+def get_autocorrelation(signal: np.ndarray, length: int) -> np.ndarray:
+    """
+    This function calculates the autocorrelation of a signal for the values in (-length, length).
+    """
     n = len(signal)
-    return np.convolve(signal, signal[::-1], mode='full')[::-1] / n
+    padded_signal = np.pad(signal, (n, n), 'constant')
+    acor = np.zeros(2*length - 1)
+    for lag in range(-length + 1, length):
+        acor[lag + length - 1] = np.sum(np.roll(padded_signal, lag) * padded_signal)
+    return acor / n
 
-def get_third_moment(signal: np.ndarray) -> np.ndarray:
-    n = len(signal) # max lag
+def get_third_moment(signal: np.ndarray, length: int) -> np.ndarray:
+
+    n = length # max lag
+
     third_moment = np.zeros((2*n - 1, 2*n - 1))
     padded_signal = np.pad(signal, (n, n), 'constant')
     
@@ -18,22 +29,34 @@ def get_third_moment(signal: np.ndarray) -> np.ndarray:
         for lag_2 in range(-n + 1, n):
             third_moment[lag_1 + n - 1, lag_2 + n - 1] = np.sum(padded_signal*np.roll(padded_signal, lag_1)*np.roll(padded_signal, lag_2))
     
-    return third_moment / n
+    return third_moment / len(signal)
 
 def add_gaussian_noise(signal: np.ndarray, sigma: int = 1) -> np.ndarray:
     noise = np.random.normal(0, sigma, np.shape(signal))
     return signal + noise
 
+def main():
+    # Generate a gaussian signal:
+    base_signal = add_gaussian_noise(np.zeros(1000), 1)
+
+    # Add noise
+    Noisy_signal = add_gaussian_noise(base_signal, 0.1)
+
+    # calculate moments:
+    mean = np.mean(Noisy_signal)
+    acor = get_autocorrelation(Noisy_signal, length=100)
+    start = time.time()
+    third_moment = get_third_moment(Noisy_signal, length=100)
+    end = time.time()
+    print(end - start)
+    print(f"The mean of the signal is: {mean}")
+    print(f"the auto correlation of the signal is: {acor}")
+    print(f"the third moment of the signal is {third_moment}")
+    print('tat')
+
+
 
 # Example usage
 if __name__ == "__main__":
-    signal = np.array([1, 2, 3])
-    mean = np.mean(signal)
-    print(mean)
-
-    acor = get_autocorrelation(signal)
-    print(acor)
-
-    third_moment = get_third_moment(signal)
-    print(third_moment)
-    print('yes')
+    main()
+    print('yay')
